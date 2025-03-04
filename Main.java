e935cf1880f9b9751428c20b6c9c20ab5948b189
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-03-04 11:45:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        int marks = scanner.nextInt();
+        
+        char grade;
+        if (marks >= 90) {
+            grade = 'A';
+        } else if (marks >= 80) {
+            grade = 'B';
+        } else if (marks >= 70) {
+            grade = 'C';
+        } else if (marks >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        
+        System.out.println("Your grade is: " + grade);
+        scanner.close();
+    }
+}
